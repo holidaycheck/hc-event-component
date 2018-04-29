13f9e3bc2094f34af4162a5c43e3b8a7962b32d3
wolframkriesing <wolfram+github@kriesing.de>
2018-04-29 18:38:32 +0200

    Insert the HC logo before the event location, if the event takes place at HC.

--- src/HcEvent.js
+++ src/HcEvent.js
@@ -1,11 +1,25 @@
-const allTags = (rootNode) => {
+const allTags = rootNode => {
   const tagNodes = rootNode.querySelectorAll('[is-tag-list] *');
   const tags = [];
   tagNodes.forEach(node => tags.push(node.innerText));
   return tags;  
 }
 
+const addressIsAtHolidayCheck = rootNode => {
+  let addressNode = rootNode.querySelector('address');
+  const addressText = addressNode ? addressNode.innerText : '';
+  return addressText.toLowerCase().includes('holidaycheck');
+}
+
 class HcEvent extends HTMLElement {
+  connectedCallback() {
+    if (addressIsAtHolidayCheck(this)) {
+      const img = new Image();
+      img.src = 'http://techblog.holidaycheck.com/img/hc-labs-only-logo.svg';
+      const addressNode = this.querySelector('address');
+      addressNode.parentNode.insertBefore(img, addressNode);
+    }
+  }
   hasTag(tagName) {
     const tags = allTags(this);
     const lowerCaseTags = tags.map(tag => tag.toLowerCase());
